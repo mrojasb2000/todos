67f2129a4ba2dc1052b5ef884c8d642b43aa3e06
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-07-26 18:40:29 -0400

    feat: added filter by id

---
+++ model.py
@@ -0,0 +1,16 @@
+"""Pydantic dependencies"""
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    """Item pydantic model"""
+
+    item: str
+    status: str
+
+
+class Todo(BaseModel):
+    """Todo pydantic model"""
+
+    id: int
+    item: Item
--- todo.py
+++ todo.py
@@ -1,19 +1,29 @@
 """Import dependencies"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
+from model import Todo
 
 todo_router = APIRouter()
 
 todo_list = []
 
 
-@todo_router.post("/todo")
-async def add_todo(todo: dict) -> dict:
-    """Add function"""
+@todo_router.post("/todo", tags=["Todos"])
+async def add_todo(todo: Todo) -> dict:
+    """Add todo to data bases"""
     todo_list.append(todo)
     return {"message": "Todo added seccessfully"}
 
 
-@todo_router.get("/todo")
+@todo_router.get("/todo", tags=["Todos"])
 async def retrieve_todos() -> dict:
-    """List function"""
+    """List todos from data bases"""
     return {"todos": todo_list}
+
+
+@todo_router.get("/todo/{todo_id}", tags=["Todos"])
+async def filter_by_id(todo_id: int = Path(ge=0, le=100)) -> dict:
+    """Get todo by id"""
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "Todo with supplied ID doesn't exist."}
